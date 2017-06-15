0b223c224cde9f52597909e2ebb5b2dc4c477630
Clandestine1 <Clandestine.abyss@gmail.com>
2017-06-15 14:10:38 -0400

    Added Bootstrap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,57 @@
-class PostsController < ApplicationController
+class PostsController < ApplicationController  
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-def index  
-end 
+  def index
+    @posts = Post.all
+  end
 
-end
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+   def create
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Story updated."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Update failed.  Please check the form."
+      render :edit
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      flash[:success] = "Story Deleted."
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Destroy failed.  Please check the form."
+      render :destory
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption, :story)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end  
