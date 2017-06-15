dd013215715e78f22cfa793f2a077d8e0bfc65fe
Clandestine1 <Clandestine.abyss@gmail.com>
2017-06-15 15:16:34 -0400

    Adding Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+
+  validates :user_name, presence: true, length: { minimum: 2, maximum: 17 }  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
