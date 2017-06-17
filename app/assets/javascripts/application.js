0a32c916b16b97c03ab2cdd9482b3e380a0a0772
Clandestine1 <Clandestine.abyss@gmail.com>
2017-06-17 09:53:54 -0400

    Added Yarn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,21 @@
 //= require masonry/jquery.loremimages.min
 //= require isotope/jquery.isotope
 //= require masonry/jquery.masonry
+//= require turbolinks
 //= require bootstrap-sprockets
+//= require bootstrap
+//= require bootstrap/affix
+//= require bootstrap/alert
+//= require bootstrap/button
+//= require bootstrap/carousel
+//= require bootstrap/collapse
+//= require bootstrap/dropdown
+//= require bootstrap/tab
+//= require bootstrap/transition
+//= require bootstrap/scrollspy
+//= require bootstrap/modal
+//= require bootstrap/tooltip
+//= require bootstrap/popover
 //= require_tree .
 
 
--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,14 +1,7 @@
-
-$ ->
-      $('.masonry-container').imagesLoaded ->
-        $('.masonry-container').masonry
-         isAnimated: true,
-  		 animationOptions: {
-  		 isFitWidth: true,
-    	 duration: 750,
-    	 easing: 'linear',
-    	 queue: false }
-          itemSelector: '.box '
-          columnWidth: (containerWidth) ->
-            if $(window).width() >= 992  //for col-md-4
-              containerWidth /3
+$(window).scroll(function() { 
+if ($(this).scrollTop()> 100) { 
+$('#wrapper').fadeIn(); 
+} else { 
+$('#wrapper').fadeOut(); 
+} 
+});
