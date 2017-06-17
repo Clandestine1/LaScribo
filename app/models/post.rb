3d09dc0d1be8e338ef85109be902dce4ced72f4e
Clandestine1 <Clandestine.abyss@gmail.com>
2017-06-17 00:31:47 -0400

    More Masonry and Bootstrap

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :image, presence: true
 
-  has_attached_file :image, styles: { :medium => "340x" } 
+  has_attached_file :image, styles: { :medium => "340x450>", :thumb => "400x" } 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
