8a2acb55391d9671847aa6135688b2c521be2c83
Clandestine1 <Clandestine.abyss@gmail.com>
2017-06-19 10:50:04 -0400

    Reverted

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-
-  devise_for :users, :controllers => { registrations: 'registrations' }
-
   get 'about/about'
-  
+
+  devise_for :users, :controllers => { registrations: 'registrations' }  
   root 'posts#index'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
